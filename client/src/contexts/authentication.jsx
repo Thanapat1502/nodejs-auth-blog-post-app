53ad00422e3dfc67321371c11a1ad81bf413b9a1
Thanapat1502 <psy150201027@gmail.com>
2024-11-18 15:29:24 +0700

    feat: add authentication feature

--- client/src/contexts/authentication.jsx
+++ client/src/contexts/authentication.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode"; //<<<<<<< npm i jwt-decode
 
 const AuthContext = React.createContext();
 
@@ -8,19 +11,38 @@ function AuthProvider(props) {
     error: null,
     user: null,
   });
+  const navigate = useNavigate();
 
-  const login = () => {
+  const login = async (data) => {
     // 🐨 Todo: Exercise #4
     //  ให้เขียน Logic ของ Function `login` ตรงนี้
     //  Function `login` ทำหน้าที่สร้าง Request ไปที่ API POST /login
     //  ที่สร้างไว้ด้านบนพร้อมกับ Body ที่กำหนดไว้ในตารางที่ออกแบบไว้
+    try {
+      //1.ส่ง credential ไป sever
+      const result = await axios.post("http://localhost:4000/auth/login", data);
+      //2.รับ token มาจาก res
+      const token = result.data.token;
+      //3.ตั้งค่า token ใน local storage
+      localStorage.setItem("token", token);
+      //4.รับ user data จาก token โดยใช้ jwtDecode(token)
+      const userDataFromToken = jwtDecode(token);
+      //5.ตั้ง state ให้มีค่าเป็น state เดิม (...state) + ข้อมูล user จาก token
+      setState({ ...state, user: userDataFromToken });
+      //6.นาวิเกต user กลับหน้าหลัก
+      navigate("/");
+    } catch (e) {
+      console.log(e);
+    }
   };
 
-  const register = () => {
+  const register = async (data) => {
     // 🐨 Todo: Exercise #2
     //  ให้เขียน Logic ของ Function `register` ตรงนี้
     //  Function register ทำหน้าที่สร้าง Request ไปที่ API POST /register
     //  ที่สร้างไว้ด้านบนพร้อมกับ Body ที่กำหนดไว้ในตารางที่ออกแบบไว้
+    await axios.post("http://localhost:4000/auth/register", data);
+    navigate("/login");
   };
 
   const logout = () => {
@@ -33,8 +55,7 @@ function AuthProvider(props) {
 
   return (
     <AuthContext.Provider
-      value={{ state, login, logout, register, isAuthenticated }}
-    >
+      value={{ state, login, logout, register, isAuthenticated }}>
       {props.children}
     </AuthContext.Provider>
   );
--- server/apps/auth.js
+++ server/apps/auth.js
@@ -1,11 +1,67 @@
 import { Router } from "express";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+import { db } from "../utils/db.js";
+import bcrypt from "bcrypt";
 
 const authRouter = Router();
 
 // 🐨 Todo: Exercise #1
 // ให้สร้าง API เพื่อเอาไว้ Register ตัว User แล้วเก็บข้อมูลไว้ใน Database ตามตารางที่ออกแบบไว้
+authRouter.post("/register", async (req, res) => {
+  //1.สร้างก้อนข้อมูล user
+  const user = {
+    username: req.body.username,
+    password: req.body.password,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+  };
+  //2.เข้ารหัสด้วย salt
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+  console.log(user);
+
+  //3.ยัด user เข้า collection
+  const collection = db.collection("users");
+  await collection.insertOne(user);
+  //4.รีเทิร์น res
+  return res.status(201).json({ message: "User has been created" });
+});
 
 // 🐨 Todo: Exercise #3
 // ให้สร้าง API เพื่อเอาไว้ Login ตัว User ตามตารางที่ออกแบบไว้
+authRouter.post("/login", async (req, res) => {
+  const collection = db.collection("users");
+  //1.validate username
+
+  const user = await collection.findOne({ username: req.body.username });
+  if (!user) {
+    return res.status(404).json({ messgae: "User not found." });
+  }
+
+  //2.validate password
+  const isValidPassword = await bcrypt.compare(
+    req.body.password,
+    user.password
+  );
+  if (!isValidPassword) {
+    return res.status(400).json({ meassge: "Password is not valid" });
+  }
+  //3.สร้างโทเคน โดยใช้ jwt.sign(payload, key, time)
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    },
+    process.env.SECRET_KEY,
+    { expiresIn: 900000 }
+  );
+
+  //4.return แนบ token
+
+  return res.status(200).json({ message: "Login Succesfully" });
+});
 
 export default authRouter;
